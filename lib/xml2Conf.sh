ee3d4cc7b65e206670b689267102b3688ffe6c20
kasrasa <kasrasadatsharifi@gmail.com>
2021-12-25 22:53:50 -0500

    initializing repository

---
+++ lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/home/kasra/anaconda3/envs/wine-project/lib"
+XML2_LIBS="-lxml2 -L/home/kasra/anaconda3/envs/wine-project/lib -lz -L/home/kasra/anaconda3/envs/wine-project/lib -llzma -lpthread -L/home/kasra/anaconda3/envs/wine-project/lib -L/home/kasra/anaconda3/envs/wine-project/lib -licui18n -licuuc -licudata -lm "
+XML2_INCLUDEDIR="-I/home/kasra/anaconda3/envs/wine-project/include/libxml2 -I/home/kasra/anaconda3/envs/wine-project/include"
+MODULE_VERSION="xml2-2.9.12"
+
